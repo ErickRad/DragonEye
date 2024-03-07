fc41b8793f4d56eacbd0f48908d368093fef0e88
Érick <contato.erickradmann@gmail.com>
2024-03-07 15:08:01 -0300

    Updates

--- main.py
+++ main.py
@@ -1,25 +1,35 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+from ttkthemes import ThemedStyle
 import backend
 
+def verify_connection():
+    result = backend.isConnected()
+    messagebox.showinfo("Network Connection", f"Network Connection: {result}")
+
+def check_devices():
+    if backend.isConnected():
+        devices = backend.scanDevices()
+        devices_info = "\n".join([f"IP: {device[0]}   MAC: {device[1]}   Brand: {device[2]}" for device in devices])
+        messagebox.showinfo("Devices on Network", devices_info)
+    else:
+        messagebox.showwarning("Warning", "Network is not connected. Please check your connection.")
+
 if __name__ == '__main__':
-    while True:
-        option = int(input('''
--------------------- DRAGON EYE ---------------------
-            
-1 - Verify your network connection
-2 - Check devices on your network
-3 - Exit
-            
-Choose an option: '''))
+    window = tk.Tk()
+    window.title("Dragon Eye")
 
-        match option:
-            case 1:
-                print(backend.isConnected())
-            case 2:
-                if backend.isConnected():
-                    devices = backend.scanDevices()
-                    for device in devices:
-                        print(f"IP: {device[0]}       MAC: {device[1]}       Brand: {device[2]}")
-            case 3:
-                break
+    # Configurar o estilo do tema escuro
+    style = ThemedStyle(window)
+    style.set_theme("equilux")
 
+    btn_verify_connection = ttk.Button(window, text="Verify Network Connection", command=verify_connection)
+    btn_verify_connection.pack(pady=10)
 
+    btn_check_devices = ttk.Button(window, text="Check Devices on Network", command=check_devices)
+    btn_check_devices.pack(pady=10)
+
+    btn_exit = ttk.Button(window, text="Exit", command=window.destroy)
+    btn_exit.pack(pady=10)
+
+    window.mainloop()
